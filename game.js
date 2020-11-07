9223697b56c9c36ff3eddcbbc5e137dbfc9359d2
Santiago Villa <santiago.villa@radiumrocket.com>
2020-11-07 03:11:34 -0300

    SNAKE: task 06. First canvas animation

--- game.js
+++ game.js
@@ -1,15 +1,43 @@
 var canvas = null,
-    ctx = null;
+    ctx = null,
+    x = 50,
+    y = 50;
+
+window.requestAnimationFrame = (function() {
+    return window.requestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        function(callback) {
+            window.setTimeout(callback, 17);
+        };
+}());
 
 function paint(ctx) {
+    ctx.fillStyle = '#000';
+    ctx.fillRect = (0, 0, canvas.width, canvas.height);
+
     ctx.fillStyle = '#0f0';
-    ctx.fillRect(50, 50, 100, 60);
+    ctx.fillRect(x, y, 10, 10);
+}
+
+function act() {
+    x += 2;
+    if (x > canvas.width) {
+        x = 0;
+    }
+}
+
+function run() {
+    window.requestAnimationFrame(run);
+    act();
+    paint(ctx);
 }
 
 function init() {
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
-    paint(ctx);
+
+    run();
 }
 
 window.addEventListener('load', init, false);
